d0b328e1c66905ef49e79225aa5c57044d8fd993
msh_intro <sriharshithmamidala5@gmail.com>
2026-09-01 02:16:48 +0530

    errors fixed

--- utils/currency_converter.py
+++ utils/currency_converter.py
@@ -2,7 +2,7 @@ import requests
 
 class CurrencyConverter:
     def __init__(self, api_key: str):
-        self.base_url = f"https://v6.exchangerate-api.com/v6/{api_key}/latest/"
+       self.base_url = f"https://v6.exchangerate-api.com/v6/{api_key}/latest"
     
     def convert(self, amount:float, from_currency:str, to_currency:str):
         """Convert the amount from one currency to another"""
